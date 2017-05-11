a0ec5c3d71a5fb553f172dc63c8ec0e248d52b46
SOUTIF Thomas <s16005532@A-F001925-L.salsa.univ-amu.fr>
2017-05-11 13:17:52 +0200

    Ajout de la classe HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+
+
+
+package fr.univ_amu.iut.exercice1;
+
+public class HelloWorld {
+
+
+	public static void main(String[] args){
+	
+	System.out.print(hello("Hello World"));
+	
+
+
+}
+
+
+
+    public static String hello(String s) {
+        
+
+		return s;
+
+    }
+}
